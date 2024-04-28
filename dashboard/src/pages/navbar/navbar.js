8158ee4d7da930fc5504210c4d9128a8976ea9f2
RCPK2023 <125800885+RCPK2023@users.noreply.github.com>
2024-04-28 14:50:21 +0800

    Ganito pala yung button

--- dashboard/src/pages/navbar/navbar.js
+++ dashboard/src/pages/navbar/navbar.js
@@ -1,9 +1,23 @@
-import { Container, Box, IconButton } from '@mui/material';
+import { Container, Box, IconButton, Popover, Typography } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
-import React from 'react';
 import './navbar.css';
+import * as React from 'react';
 
-const navbar = () => {
+const Navbar = () => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <div id="container">
       <Container id="form-container-for-navbar">
@@ -11,16 +25,32 @@ const navbar = () => {
     {/* Ganito nalang format for each buttons */}
       
           <IconButton
-            href="/"
+           
             variant="text"
-            id="navbar-button">
+            id="navbar-button"
+            onClick={handleClick}>
                 <HomeIcon/>
           </IconButton>
-        
+          <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'center',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'center',
+          horizontal: 'left',
+        }}
+      >
+        <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
+      </Popover>
 
       </Container>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
